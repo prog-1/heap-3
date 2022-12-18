ead6bf815f834f4aeecc10b56775fa5a7029b264
Littleunidragon <81423410+Littleunidragon@users.noreply.github.com>
2022-12-19 01:19:15 +0200

    Cleaning junk from heapify tests

--- heapify/heapify_test.go
+++ heapify/heapify_test.go
@@ -18,21 +18,20 @@ func isHeap(s []int, i int) bool {
 func TestHeapify(t *testing.T) {
 	for _, tc := range []struct {
 		s    []int
-		want []int
 	}{
 		// we dont really care about exact values here
 		// only if result is max heap to sort array correctly
-		{nil, nil},
-		{[]int{}, []int{}},
-		{[]int{0, 0}, []int{0, 0}},
-		{[]int{9}, []int{9}},
-		{[]int{4, 5, 3}, []int{3, 4, 5}},
-		{[]int{6, 1, 5, 9, 4, 2, 3, 7, 8}, []int{8}},
-		{[]int{6, 0, 1, 5, 9, 4, 2, 3, 7, 8}, []int{9}},
+		{nil},
+		{[]int{}},
+		{[]int{0, 0}},
+		{[]int{9}},
+		{[]int{4, 5, 3}},
+		{[]int{6, 1, 5, 9, 4, 2, 3, 7, 8}},
+		{[]int{6, 0, 1, 5, 9, 4, 2, 3, 7, 8}},
 	} {
 		t.Run("", func(t *testing.T) {
-			if Heapify(tc.s); !reflect.DeepEqual(tc.s, tc.want) {
-				t.Errorf("s = %v, want = %v, but != max heap", tc.s, tc.want)
+			if Heapify(tc.s);!isHeap(tc.s){
+				t.Errorf("s = %v, but != max heap", tc.s)
 			}
 		})
 	}
